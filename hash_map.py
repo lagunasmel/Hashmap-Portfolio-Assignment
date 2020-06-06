108043f1725b0bc17e6306d4cfff977cfc4f4993
Melissa Lagunas <lagunasmel@gmail.com>
2020-06-05 20:36:33 -0400

    Initial commit

---
+++ hash_map.py
@@ -0,0 +1,305 @@
+# hash_map.py
+# ===================================================
+# Implement a hash map with chaining
+# ===================================================
+
+
+class SLNode:
+    def __init__(self, key, value):
+        self.next = None
+        self.key = key
+        self.value = value
+
+    def __str__(self):
+        return '(' + str(self.key) + ', ' + str(self.value) + ')'
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def add_front(self, key, value):
+        """Create a new node and inserts it at the front of the linked list
+        Args:
+            key: the key for the new node
+            value: the value for the new node"""
+        new_node = SLNode(key, value)
+        new_node.next = self.head
+        self.head = new_node
+        self.size = self.size + 1
+
+    def remove(self, key):
+        """Removes node from linked list
+        Args:
+            key: key of the node to remove """
+        if self.head is None:
+            return False
+        if self.head.key == key:
+            self.head = self.head.next
+            self.size = self.size - 1
+            return True
+        cur = self.head.next
+        prev = self.head
+        while cur is not None:
+            if cur.key == key:
+                prev.next = cur.next
+                self.size = self.size - 1
+                return True
+            prev = cur
+            cur = cur.next
+        return False
+
+    def contains(self, key):
+        """Searches linked list for a node with a given key
+        Args:
+            key: key of node
+        Return:
+            node with matching key, otherwise None"""
+        if self.head is not None:
+            cur = self.head
+            while cur is not None:
+                if cur.key == key:
+                    return cur
+                cur = cur.next
+        return None
+
+    def __str__(self):
+        out = '['
+        if self.head is not None:
+            cur = self.head
+            out = out + str(self.head)
+            cur = cur.next
+            while cur is not None:
+                out = out + ' -> ' + str(cur)
+                cur = cur.next
+        out = out + ']'
+        return out
+
+
+def hash_function_1(key):
+    hash = 0
+    for i in key:
+        hash = hash + ord(i)
+    return hash
+
+
+def hash_function_2(key):
+    hash = 0
+    index = 0
+    for i in key:
+        hash = hash + (index + 1) * ord(i)
+        index = index + 1
+    return hash
+
+
+class HashMap:
+    """
+    Creates a new hash map with the specified number of buckets.
+    Args:
+        capacity: the total number of buckets to be created in the hash table
+        function: the hash function to use for hashing values
+    """
+
+    def __init__(self, capacity, function):
+        self._buckets = []
+        for i in range(capacity):
+            self._buckets.append(LinkedList())
+        self.capacity = capacity
+        self._hash_function = function
+        self.size = 0
+
+    def clear(self):
+        """
+        Empties out the hash table deleting all links in the hash table.
+        """
+        self._buckets = []
+        for i in range(self.capacity):
+            self._buckets.append(LinkedList())
+        self.size = 0
+
+
+    def get(self, key):
+        """
+        Returns the value with the given key.
+        Args:
+            key: the value of the key to look for
+        Return:
+            The value associated to the key. None if the link isn't found.
+        """
+        # Find the index
+        index = self._hash_function(key) % self.capacity
+        # Grab the node
+        node = self._buckets[index].contains(key)
+        # If the link
+        if node is not None:
+            return node.value
+        else:
+            return None
+
+    def get_buckets(self):
+        """Returns the buckets list."""
+        return self._buckets
+
+    def resize_table(self, capacity):
+        """
+        Re-sizes the hash table to have a number of buckets equal to the given
+        capacity. All links need to be rehashed in this function after resizing
+        Args:
+            capacity: the new number of buckets.
+        """
+        # Create a new array and initiate new linked lists
+        temp = []
+        for num in range(capacity):
+            temp.append(LinkedList())
+
+        # Iterate through each linked list in the previous array and retrieve new hashes
+        # Add these into new existing array
+        for bucket in self._buckets:
+            if bucket.head is not None:
+                curr = bucket.head
+                while curr is not None:
+                    new_index = self._hash_function(curr.key) % capacity
+                    temp[new_index].add_front(curr.key, curr.value)
+                    curr = curr.next
+        # Update buckets to point to new array
+        self._buckets = temp
+        # Update to new capacity
+        self.capacity = capacity
+
+
+    def put(self, key, value):
+        """
+        Updates the given key-value pair in the hash table. If a link with the given
+        key already exists, this will just update the value and skip traversing. Otherwise,
+        it will create a new link with the given key and value and add it to the table
+        bucket's linked list.
+
+        Args:
+            key: they key to use to has the entry
+            value: the value associated with the entry
+        """
+        index = self._hash_function(key) % self.capacity
+        node = self._buckets[index].contains(key)
+        # Point to the linked list in this index
+        if node is not None:
+            node.value = value
+        else:
+            self._buckets[index].add_front(key, value)
+            self.size += 1
+
+    def remove(self, key):
+        """
+        Removes and frees the link with the given key from the table. If no such link
+        exists, this does nothing. Remember to search the entire linked list at the
+        bucket.
+        Args:
+            key: they key to search for and remove along with its value
+        """
+        index = self._hash_function(key) % self.capacity
+
+        if self.contains_key(key) is True:
+            self._buckets[index].remove(key)
+            self.size -= 1
+        else:
+            return
+
+
+    def contains_key(self, key: object) -> object:
+        """
+        Searches to see if a key exists within the hash table
+
+        Returns:
+            True if the key is found False otherwise
+
+        """
+        index = self._hash_function(key) % self.capacity
+        if self._buckets[index].contains(key) is not None:
+            return True
+        else:
+            return False
+
+    def empty_buckets(self):
+        """
+        Returns:
+            The number of empty buckets in the table
+        """
+        count = 0
+        for bucket in self._buckets:
+            if bucket.head is None:
+                count += 1
+        return count
+
+
+    def table_load(self):
+        """
+        Returns:
+            the ratio of (number of links) / (number of buckets) in the table as a float.
+
+        """
+        return float(self.size/self.capacity)
+
+    def __str__(self):
+        """
+        Prints all the links in each of the buckets in the table.
+        """
+
+        out = ""
+        index = 0
+        for bucket in self._buckets:
+            out = out + str(index) + ': ' + str(bucket) + '\n'
+            index = index + 1
+        return out
+
+
+# test = HashMap(50, hash_function_1)
+# print(test.empty_buckets())
+# print(test.contains_key('key1'))
+
+# for i in range(150):
+#     test.put('str' + str(i), i*100)
+#     if i % 25 == 24:
+#         print(test.empty_buckets(), test.size, test.capacity)
+
+
+# m = HashMap(20, hash_function_1)
+# m.put('key1', 10)
+# print(m.size, m.capacity, m.get('key1'), m.contains_key('key1'))
+# m.resize_table(30)
+# print(m.size, m.capacity, m.get('key1'), m.contains_key('key1'))
+
+# m = HashMap(75, hash_function_2)
+# keys = [i for i in range(1, 1000, 13)]
+#
+# for key in keys:
+#     m.put(str(key), key * 42)
+# print(m.size, m.capacity)
+# for capacity in range(111, 1000, 117):
+#     m.resize_table(capacity)
+#     result = True
+#     for key in keys:
+#         result = result and m.contains_key(str(key))
+#         result = result and not m.contains_key(str(key + 1))
+#     print(capacity, result, m.size, m.capacity, round(m.table_load(), 2))
+#
+
+# m = HashMap(75, hash_function_2)
+# keys = [i for i in range(1, 1000, 13)]
+# for key in keys:
+#     m.put(str(key), key * 42)
+# print(m.size, m.capacity)
+# for capacity in range(111, 1000, 117):
+#     m.resize_table(capacity)
+#     result = True
+#     for key in keys:
+#         result = result and m.contains_key(str(key))
+#         result = result and not m.contains_key(str(key + 1))
+#     print(capacity, result, m.size, m.capacity, round(m.table_load(), 2))
+
+# m = HashMap(50, hash_function_1)
+# print(m.get('key1'))
+# m.put('key1', 10)
+# print(m.get('key1'))
+# m.remove('key1')
+# print(m.get('key1'))
+# m.remove('key4')
---
+++ word_count.py
@@ -0,0 +1,80 @@
+# word_count.py
+# ===================================================
+# Implement a word counter that counts the number of
+# occurrences of all the words in a file. The word
+# counter will return the top X words, as indicated
+# by the user.
+# ===================================================
+
+import re
+from hash_map import HashMap
+
+"""
+This is the regular expression used to capture words. It could probably be endlessly
+tweaked to catch more words, but this provides a standard we can test against, so don't
+modify it for your assignment submission.
+"""
+rgx = re.compile("(\w[\w']*\w|\w)")
+
+
+def hash_function_2(key):
+    """
+    This is a hash function that can be used for the hashmap.
+    """
+
+    hash = 0
+    index = 0
+    for i in key:
+        hash = hash + (index + 1) * ord(i)
+        index = index + 1
+    return hash
+
+
+def top_words(source, number):
+    """
+    Takes a plain text file and counts the number of occurrences of case insensitive words.
+    Returns the top `number` of words in a list of tuples of the form (word, count).
+
+    Args:
+        source: the file name containing the text
+        number: the number of top results to return (e.g. 5 would return the 5 most common words)
+    Returns:
+        A list of tuples of the form (word, count), sorted by most common word. (e.g. [("a", 23), ("the", 20), ("it", 10)])
+    """
+
+    keys = set()
+
+    ht = HashMap(2500, hash_function_2)
+
+    # This block of code will read a file one word at a time and
+    # put the word in `w`. It should be left as starter code.
+
+    with open(source) as f:
+        for line in f:
+            words = rgx.findall(line)
+            for w in words:
+                w = w.lower()
+                # If the word is already in the hash map, pass the value with a new updated count
+                if ht.contains_key(w) is False:
+                    ht.put(w, 1)
+                else:
+                    count = ht.get(w) + 1
+                    ht.put(w, count)
+
+    # Add all of the words to the keys set
+    for bucket in ht.get_buckets():
+        curr = bucket.head
+        while curr is not None:
+            keys.add((curr.key, curr.value))
+            curr = curr.next
+
+
+    all_words = list(keys)
+    all_words.sort(key=lambda word: word[1])
+    slice_val = (number * -1 - 1)
+    top_wds = all_words[:slice_val:-1]
+
+    return top_wds
+
+
+print(top_words("alice.txt", 10))  # COMMENT THIS OUT WHEN SUBMITTING TO GRADESCOPE
